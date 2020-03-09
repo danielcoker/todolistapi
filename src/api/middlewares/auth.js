8f85df9280751610fe71fba4929efc23439bf6db
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-03-09 21:39:46 +0100

    Create protect middleware for token authentication

---
+++ src/api/middlewares/auth.js
@@ -0,0 +1,49 @@
+import jwt from 'jsonwebtoken';
+import ErrorResponse from '../utils/errorResponse';
+import User from '../models/User';
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: 'You are not authorized to access this route.'
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'You are not authorized to access this route.'
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return res.status(401).json({
+        success: false,
+        message: 'Token expired.'
+      });
+    }
+    return res.status(401).json({
+      success: false,
+      message: 'Unable to authenticate token.'
+    });
+  }
+};
+
+export { protect };
