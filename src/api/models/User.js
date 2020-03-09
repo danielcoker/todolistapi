b649f8896126340ce7a647907045374f97f60b99
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-03-09 09:02:52 +0100

    Create User model

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -1,15 +1,34 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
-  phone: { type: String, required: true },
-  password: { type: String, required: true }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  password: { type: String, required: true, select: false }
 });
+
+/**
+ * @desc Hash password using bcryptjs before saving user to database.
+ */
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 const User = mongoose.model('User', UserSchema);
 
-export default {
-  User
-};
+export default User;
