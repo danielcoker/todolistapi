28ebeb93ffa60f24615621ca50c8f92e3c6133a7
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-03-11 14:26:37 +0100

    Change http method from get to post

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -3,7 +3,7 @@ import controllers from '../controllers';
 
 const router = express.Router();
 
-router.get('/register', controllers.auth.register);
-router.get('/login', controllers.auth.login);
+router.post('/register', controllers.auth.register);
+router.post('/login', controllers.auth.login);
 
 export default router;
