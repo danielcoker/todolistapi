19db98ed2497c94a0092e4164fd14daff09d772e
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-03-14 21:07:15 +0100

    Add protect middleware to create task route

--- src/api/routes/tasks.js
+++ src/api/routes/tasks.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import controllers from '../controllers';
+import { protect } from '../middlewares/auth';
 
 const router = express.Router();
 
 router
   .route('/')
   .get(controllers.tasks.getTasks)
-  .post(controllers.tasks.createTask);
+  .post(protect, controllers.tasks.createTask);
 
 export default router;
